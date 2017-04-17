af11c97d4a3052504790e98493d2d87a2f1c4211
Khristan Crooms <kbcrooms@localhost.localdomain>
2017-04-17 15:45:13 -0400

    Modified Hashtable and methods to accept new Student data type

--- Student.java
+++ Student.java
@@ -3,11 +3,14 @@ class Student{
   String password;
   String major;
   String rank;
-  Student(  String username,String password,String major,String rank){
+  String college;
+  String stance;
+  Student(  String username,String password,String major,String rank,String college){
     this.username = username;
     this.password = password;
     this.major = major;
     this.rank = rank;
+    this.college = college;
   }
 
 }
