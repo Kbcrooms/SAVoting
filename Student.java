57ced9bb7adc9d587240b3b2a0d58bcae17ff2dc
Khristan Crooms <kbcrooms@localhost.localdomain>
2017-04-17 15:21:20 -0400

    added basic student class

---
+++ Student.java
@@ -0,0 +1,13 @@
+class Student{
+  String username;
+  String password;
+  String major;
+  String rank;
+  Student(  String username,String password,String major,String rank){
+    this.username = username;
+    this.password = password;
+    this.major = major;
+    this.rank = rank;
+  }
+
+}
